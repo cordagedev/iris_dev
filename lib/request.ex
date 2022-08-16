d6eca251d0b0a2fb958d3867a9241549555550ca
Juan Alcantara <juan@cordage.io>
2022-08-16 10:47:47 -0500

    rigging url restored

--- lib/request.ex
+++ lib/request.ex
@@ -1,9 +1,10 @@
 defmodule Iris.Request do
   defmacro __using__(_module) do
     quote do
+      @url_rigging System.get_env("RIGGING_URL")
+
       def request(:rigging, url, token) do
-        url_rigging = System.get_env("RIGGING_URL")
-        make_request("#{url_rigging}#{url}", token)
+        make_request("#{@url_rigging}#{url}", token)
       end
 
       defp make_request(url, token) do
