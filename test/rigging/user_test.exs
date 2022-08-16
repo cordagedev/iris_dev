8f1ea0e61198fad71925443ab7f88603afe3b9b8
Manuel Luna <manuellunada@gmail.com>
2022-08-16 14:45:56 -0500

    user docs

--- test/rigging/user_test.exs
+++ test/rigging/user_test.exs
@@ -1,3 +1,43 @@
 defmodule UserTest do
   use ExUnit.Case, async: true
+
+  import Mock
+
+  describe "get/2" do
+    test_with_mock "returns a user",
+                   HTTPoison,
+                   get: &user/1 do
+    end
+
+    test_with_mock "returns {:error, %UnauthorizedError{}} if token is invalid",
+                   HTTPoison,
+                   get: &user/1 do
+    end
+
+    test_with_mock "returns {:error, %NotFoundError{}} if no id matches",
+                   HTTPoison,
+                   get: &user/1 do
+    end
+  end
+
+  describe "get!/2" do
+    test_with_mock "returns a user",
+                   HTTPoison,
+                   get: &user/1 do
+    end
+
+    test_with_mock "raises UnauthorizedError if token is invalid",
+                   HTTPoison,
+                   get: &user/1 do
+    end
+
+    test_with_mock "raises NotFoundError if no id matches",
+                   HTTPoison,
+                   get: &user/1 do
+    end
+  end
+
+  defp user(_) do
+    %{}
+  end
 end
