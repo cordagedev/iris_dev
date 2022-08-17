e8618183e567e4db628fd7f02a99b8cf451d0266
Juan Alcantara <juan@cordage.io>
2022-08-17 04:09:03 +0000

    working!

--- lib/request.ex
+++ lib/request.ex
@@ -1,8 +1,6 @@
 defmodule Iris.Request do
-  @rigging_url System.get_env("RIGGING_URL")
-
   def request(:rigging, url, token) do
-    make_request("#{System.get_env("RIGGING_URL")}#{url}" |> IO.inspect(), token)
+    make_request("#{System.get_env("RIGGING_URL")}#{url}", token)
   end
 
   defp make_request(url, token) do
