dd22776a056cd49c131a1a327c9cd5b77f3c71ed
Juan Alcantara <juan@cordage.io>
2022-08-18 09:31:30 -0500

    raising for validate added

--- lib/rigging/business_unit.ex
+++ lib/rigging/business_unit.ex
@@ -104,20 +104,56 @@ defmodule Iris.Rigging.BusinessUnit do
   Examples
 
       iex> BusinessUnit.foreign_business_units_keys("my valid token", ["my id"])
-      iex> :ok
+      iex> [%Iris.Rigging.BusinessUnit{}]
 
       iex> BusinessUnit.foreign_business_units_keys("my valid token", "[not_existing_id]")
       iex> :not_found
   """
   @spec foreign_business_units_keys(user_token :: String.t(), ids :: [String.t()]) ::
-          :ok | :not_found
+          [ %__MODULE__{}] | :not_found
   def foreign_business_units_keys(user_token, ids) when is_list(ids) do
-    Enum.reduce_while(ids, :ok, fn id, acc ->
-      business_unit = get(user_token, id)
+    Enum.reduce_while(ids, [], fn id, acc ->
+      business_unit =
+        user_token
+        |> get(id)
+        |> foreign_errors()
 
-      if :error != foreign_errors(business_unit),
-        do: {:cont, acc},
+      if :error != business_unit,
+        do: {:cont, acc ++ [business_unit]},
         else: {:halt, :not_found}
+    end)
+  end
+
+  @doc """
+  Validates if a list of *Business Units* from *Rigging* exist.
+  Expects the token of the user making the request, along with
+  the ID's of the business units they are validating.
+
+  > Token must *not* contain the "Bearer" prefix.
+  > Also, the authorization login will be enforce
+  > by *Rigging*.
+
+  Raises a `Iris.Error.NotFoundError` if no record is matched.
+
+  Raises a `Iris.Error.UnauthorizedError` if *Rigging* sends an "unauthorized" response.
+
+  Examples
+
+      iex> BusinessUnit.foreign_business_units_keys!("my valid token", ["my id"])
+      iex> [%Iris.Rigging.BusinessUnit{}]
+
+      iex> BusinessUnit.foreign_business_units_keys!("my valid token", "[not_existing_id]")
+      iex> ** Iris.Error.NotFoundError
+
+      iex> BusinessUnit.foreign_business_units_keys!("my invalid token", ["my id"])
+      iex> ** Iris.Error.UnauthorizedError
+  """
+  @spec foreign_business_units_keys!(user_token :: String.t(), ids :: [String.t()]) ::
+         [ %__MODULE__{}]
+  def foreign_business_units_keys!(user_token, ids) when is_list(ids) do
+    Enum.reduce(ids, [], fn id, acc ->
+      business_unit = get!(user_token, id)
+      acc ++ [business_unit]
     end)
   end
 
@@ -127,5 +163,5 @@ defmodule Iris.Rigging.BusinessUnit do
   defp parse({:ok, body}), do: {:ok, struct(__MODULE__, body)}
 
   defp foreign_errors({:error, _}), do: :error
-  defp foreign_errors(_), do: nil
+  defp foreign_errors({:ok, business_unit}), do: business_unit
 end
