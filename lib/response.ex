3ff0a775953c3b9914838e3a080f1e23beec7edb
Manuel Luna <manuellunada@gmail.com>
2022-08-19 17:56:10 -0500

    all

--- lib/response.ex
+++ lib/response.ex
@@ -1,9 +1,13 @@
 defmodule Iris.Response do
+  alias Iris.Error
+
   def return({:error, error}) do
     {:error, error}
   end
 
   def return({:ok, res}), do: {:ok, res}
+
+  def return!({:error, :not_found}), do: raise Error.NotFoundError
 
   def return!({:error, error}) do
     raise error
--- lib/rigging/organization.ex
+++ lib/rigging/organization.ex
@@ -84,7 +84,33 @@ defmodule Iris.Rigging.Organization do
     return!(get(user_token, id))
   end
 
+  def all(user_token, ids) when is_list(ids) do
+    ids
+    |> Enum.map(&Task.async(fn -> get(user_token, &1) end))
+    |> Enum.map(&Task.await/1)
+    |> format_list()
+    |> return()
+  end
+
+  def all!(user_token, ids) when is_list(ids), do: return!(all(user_token, ids))
+
   # --- Private ----------------------------------------------------------------
+
+  # probablemente ahora sí sea bueno meter el tools para mover ésta
+  # y la de extract_data ahí, todos los modulos van a usar las mismas
+  defp format_list(results) do
+    # this will return two lists:
+    # the first one will be all *found* items
+    # the second one will be the ones that returned an error
+    Enum.split_with(results, fn {status, _data} -> :ok == status end)
+    |> extract_data()
+  end
+
+  def extract_data({oks, _errors = []}) do
+    {:ok, Enum.map(oks, fn {_status, data} -> data end)}
+  end
+
+  def extract_data({_oks, _errors}), do: {:error, :not_found}
 
   defp parse({:error, error}), do: {:error, error}
   defp parse({:ok, body}), do: {:ok, struct(__MODULE__, body)}
