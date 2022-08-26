1cf22be79dc0d95d3b0aed706d68b26b5e47e9fd
Manuel Luna <manuellunada@gmail.com>
2022-08-26 10:11:53 -0500

    remove log

--- lib/request.ex
+++ lib/request.ex
@@ -2,7 +2,6 @@ defmodule Iris.Request do
   def request(:rigging, url, token), do: make_request("#{rigging_url()}#{url}", token)
 
   defp make_request(url, token) do
-    IO.inspect(token)
     case HTTPoison.get(url, %{"Authorization" => "Bearer " <> token}) do
       {:error, _error} ->
         {:error, %Iris.Error.ServerError{}}
